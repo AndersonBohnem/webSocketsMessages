58882a2d58cce1141730728ec4d35ddacd274546
Anderson Bohnemberger <bohnembergeranderson@gmail.com>
2024-11-04 19:27:29 -0300

    feat: criação de método de regristro, ajuste no login, mudança na forma como é feito o login. A autenticação do NewChat agora ocorre em tempo de execução e salvo em uma lista, e modificação do backend para atender as funcionalidades do login/registro

--- js/login.js
+++ js/login.js
@@ -1,3 +1,4 @@
+/*
 const validUsers = {
     anderson: "anderson",
     luis: "luis"
@@ -14,5 +15,50 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
         localStorage.setItem("userCurrent", username);
     } else {
         alert('Usuário ou senha incorretos.');
+    }
+});
+*/
+document.getElementById("registerButton").addEventListener("click", function() {
+    window.location.href = "register.html";
+});
+
+
+/*   dadw */ 
+
+const socket = new WebSocket('ws://localhost:8888/login');
+
+function loginUser(username, password) {
+    const loginData = {
+        username: username,
+        password: password
+    };
+
+    socket.send(JSON.stringify(loginData));
+}
+
+socket.addEventListener('open', (event) => {
+    console.log('Conexão WebSocket estabelecida');
+});
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    localStorage.setItem("userCurrent", username);
+    loginUser(username, password);
+});
+
+socket.addEventListener('message', (event) => {
+    const response = JSON.parse(event.data);
+    errorMessage.textContent = '';
+    console.log("response" , response)
+
+    if (response.status === 'success') {
+        window.location.href = "home.html";
+    } else if (response.status === 'error') {
+        errorMessage.textContent = response.message;
+        errorMessage.style.color = 'red';
     }
 });
--- serverComunicacao.py
+++ serverComunicacao.py
@@ -3,6 +3,7 @@ import websockets
 import json
 
 active_chats = {}
+active_users = {}
 
 async def handler(websocket, path):
     print(f"Novo cliente conectado em {path}")
@@ -15,15 +16,48 @@ async def handler(websocket, path):
 
     try:
         async for message in websocket:
+            
             data = json.loads(message)
-            chat = data['chat'].lstrip('/')
-            msg = data['message']
+
+            if path == "login":
+                username = data['username']
+                password = data['password']
             
-            print(f"Mensagem recebida: {msg} no chat: {path}")
+                if username in active_users and active_users[username] == password:
+                    response = {"status": "success", "message": "Login successful."}
+                else:
+                    response = {"status": "error", "message": "Invalid username or password."}
             
-            for client in active_chats[path]:
-                if client != websocket:
-                    await client.send(json.dumps({"chat": path, "message": msg}))
+                print("response login:", response)
+                await websocket.send(json.dumps(response))
+                continue
+
+            
+            elif path == "register":
+                username = data['username']
+                password = data['password']
+                
+                if username in active_users:
+                    response = {"status": "error", "message": "User already exists."}
+                    print("response cadastro: " , response)
+                else:
+                    active_users[username] = password
+                    response = {"status": "success", "message": "User registered successfully."}
+                    print("response cadastro: " , response)
+                
+                await websocket.send(json.dumps(response))
+                continue
+
+            else:
+                chat = data['chat'].lstrip('/')
+                msg = data['message']
+            
+                print(f"Mensagem recebida: {msg} no chat: {path}")
+            
+                for client in active_chats[path]:
+                    if client != websocket:
+                        await client.send(json.dumps({"chat": path, "message": msg}))
+            
 
 
     except websockets.exceptions.ConnectionClosed:
